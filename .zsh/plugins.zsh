a8e2af092bae9393522e8f86f2b3bd497e096499
Kevin To <kevin.to@gmail.com>
2026-02-27 21:02:26 -0500

    🔧 Fix configs
    fix slow zsh plugin path evaluation, fiz termianal statusline

--- .zsh/plugins.zsh
+++ .zsh/plugins.zsh
@@ -1,2 +1,2 @@
 source /run/current-system/sw/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
-source $(find /nix/store -name "zsh-syntax-highlighting.zsh" 2>/dev/null | head -1)
+source /run/current-system/sw/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
